c50933c52c339a403df63505cc4017d00763cdf3
sudhakar patil <sudhakarpatil500@gmail.com>
2022-12-12 16:55:58 -0800

    added spaces

--- src/main/java/listener/Listener.java
+++ src/main/java/listener/Listener.java
@@ -8,15 +8,20 @@ import org.testng.Reporter;
 
 import mavenBase.BaseNew;
 
-public class Listener extends BaseNew implements ITestListener {
+public class Listener extends BaseNew implements ITestListener 
+    
+    {
+	
 	public void onTestSuccess(ITestResult result) 
 	{
 	Reporter.log("TC "+result.getName()+" is completed successfuly",true);
 	}
+	
 	public void onTestFailure(ITestResult result) 
 	{
 	Reporter.log("TC "+result.getName()+" is failed",true);
 	}
+	
 	public void onTestSkipped(ITestResult result) 
 	{
 	Reporter.log("TC "+result.getName()+" is skipped please check",true);
